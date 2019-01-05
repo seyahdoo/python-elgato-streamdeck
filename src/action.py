18a568df366999434a4441023e58dddd1b1ea53c
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-01-05 13:22:04 +0300

    set visible

--- src/action.py
+++ src/action.py
@@ -1,19 +1,29 @@
 
 
-
-
-
 class Action:
 
     def __init__(self):
 
         self.image = None
+        self.is_visible = False
+        self.current_space = 0
         self.is_pressed = False
         
         return
 
-    def draw(self, space_index):
-        
+    def visible(self,space):
+        self.is_visible = True
+        self.current_space = space
+        self.draw()
+
+    def invisible(self):
+        self.is_visible = False
+
+    def draw(self):
+        if self.is_visible:
+            # TODO draw to current space
+            pass
+
         return
 
     def on_pressed(self):
