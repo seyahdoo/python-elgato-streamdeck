0f816db3ba8c971192c4bc739773ffb4531980b0
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-01-05 12:37:44 +0300

    preperly call super

--- src/open_folder_action.py
+++ src/open_folder_action.py
@@ -1,20 +1,18 @@
 
+from .action import Action
+from .folder import Folder
 
-from action import Action
-from folder import Folder
 
 class OpenFolderAction(Action):
 
     def __init__(self, folder):
-        super.__init__(self)
-
+        super(OpenFolderAction,self).__init__(self)
         self.folder: Folder = folder
 
         return
 
-
     def on_pressed(self):
-        folder.open()
+        self.folder.open()
 
         return
 
